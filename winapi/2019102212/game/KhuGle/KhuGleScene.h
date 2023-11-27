7835d6a3ba5feca2a676b7553d06df1aaa9d4f9e
jeong-ho-lee <owiequhf@khu.ac.kr>
2023-11-27 19:12:18 +0900

    new files

---
+++ winapi/2019102212/game/KhuGle/KhuGleScene.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "KhuGleBase.h"
+#include "KhuGleComponent.h"
+
+class CKhuGleScene : public CKhuGleComponent
+{
+public:
+	bool m_bInit;
+	int m_nW, m_nH;
+
+	unsigned char **m_ImageR, **m_ImageG, **m_ImageB;
+	KgColor24 m_bgColor;
+
+	CKhuGleScene(int nW, int nH, KgColor24 bgColor);
+	~CKhuGleScene();
+
+	void SetBackgroundImage(int nW, int nH, KgColor24 bgColor);
+	void ResetBackgroundImage();
+	void SetBgColor(KgColor24 bgColor);
+
+	virtual void Render();
+};
+
